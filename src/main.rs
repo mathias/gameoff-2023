eee883cdc7bded06ed6afb076df33af8b5f023cc
Matt Gauger <matt.gauger@gmail.com>
2023-11-02 20:20:23 -0500

    Camera tweaking, mouse position in world, exiting when ESC pressed

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,15 @@ fn setup(c: &mut EngineContext) {
         // &[u8] with the image data.
         include_bytes!("assets/comfy.png")
     );
+    let mut camera = main_camera_mut();
+    camera.zoom = 40.0;
 }
 
-fn update(_c: &mut EngineContext) {
-    draw_sprite(
-        // Sprites are referenced with their string name.
-        // Comfy doesn't use asset handles.
-        texture_id("comfy"),
-        // position
-        Vec2::ZERO,
-        // color tint
-        WHITE,
-        // z_index
-        0,
-        // size
-        splat(5.0),
-    );
+fn update(c: &mut EngineContext) {
+    if is_key_pressed(KeyCode::Escape) {
+        *c.quit_flag = true;
+    }
+
+    let our_mouse_world = mouse_world(); //screen_to_world(mouse_position());
+    draw_text(format!("X:{} Y: {}", our_mouse_world.x, our_mouse_world.y).as_str(), Vec2::ZERO, RED, TextAlign::Center);
 }
