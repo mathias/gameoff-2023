b98b3c6cff26544f1581ac75069276643b0dee1a
Matt Gauger <matt.gauger@gmail.com>
2023-11-02 19:50:59 -0500

    Sprite example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use comfy::*;
 
-simple_game!("Nice red circle", update);
+simple_game!("Sprite Example", setup, update);
+
+fn setup(c: &mut EngineContext) {
+    c.load_texture_from_bytes(
+        // Name of our sprite
+        "comfy",
+        // &[u8] with the image data.
+        include_bytes!("assets/comfy.png")
+    );
+}
 
 fn update(_c: &mut EngineContext) {
-   draw_circle(vec2(0.0, 0.0), 0.5, RED, 0);
+    draw_sprite(
+        // Sprites are referenced with their string name.
+        // Comfy doesn't use asset handles.
+        texture_id("comfy"),
+        // position
+        Vec2::ZERO,
+        // color tint
+        WHITE,
+        // z_index
+        0,
+        // size
+        splat(5.0),
+    );
 }
